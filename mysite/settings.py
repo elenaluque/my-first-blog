40b14c326c5420664a1cc6d1c7794edb5037a41a
Elena <elenaluque12@gmail.com>
2019-03-09 12:25:55 +0100

    Updated allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ak$b=yao=x0$++n&v&t(i@94)le(-d^%rs2i7t#mf4x(ueif*('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', ' . pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'elenaluque.pythonanywhere.com']
 
 
 # Application definition
